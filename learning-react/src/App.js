72bdb7ccf022fc635079b027b9f684f44fb5472a
jozua palandi <jozuafp@gmail.com>
2020-05-17 01:56:26 +0700

    Adding project folder

---
+++ learning-react/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Learning REACT</h1>
+      <h2>Belajar yang mudah-mudah dulu</h2>
+      <h3>By: JOZ</h3>
+    </div>
+  );
+}
